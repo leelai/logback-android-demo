73fc90ecf4c34f63f483608b39a36012830e1ecc
leelai <leelai.tw@gmail.com>
2016-07-13 15:09:54 +0800

    first version

---
+++ app/src/main/java/tw/leelai/logbackdemo/MainActivity.java
@@ -0,0 +1,150 @@
+package tw.leelai.logbackdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.android.LogcatAppender;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.FileAppender;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String FOLDER_NAME = "logback";
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    createFolder();
+                    configureLogbackDirectly();
+                    logTest();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void checkPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            configureLogbackDirectly();
+            logTest();
+        }
+    }
+
+    public static File createFolder() {
+        File root = new File(Environment.getExternalStorageDirectory(),
+                FOLDER_NAME);
+        if (!root.isDirectory()) {
+            if (root.mkdirs()) {
+                Log.d(TAG, "mkdir success!");
+            } else {
+                Log.d(TAG, "mkdir fail!");
+            }
+        }
+        return root;
+    }
+
+    private void configureLogbackDirectly() {
+        Log.d(TAG, "configureLogbackDirectly");
+        // reset the default context (which may already have been initialized)
+        // since we want to reconfigure it
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        lc.reset();
+
+        // setup FileAppender
+        PatternLayoutEncoder encoder1 = new PatternLayoutEncoder();
+        encoder1.setContext(lc);
+        encoder1.setPattern("%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
+        encoder1.start();
+
+        String file = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + FOLDER_NAME + File.separator +"app.log";
+        Log.d(TAG, "write file to " + file);
+        FileAppender<ILoggingEvent> fileAppender = new FileAppender<ILoggingEvent>();
+        fileAppender.setContext(lc);
+        fileAppender.setFile(file);
+        fileAppender.setEncoder(encoder1);
+        fileAppender.start();
+
+        // setup LogcatAppender
+        PatternLayoutEncoder encoder2 = new PatternLayoutEncoder();
+        encoder2.setContext(lc);
+        encoder2.setPattern("[%thread] %msg%n");
+        encoder2.start();
+
+        LogcatAppender logcatAppender = new LogcatAppender();
+        logcatAppender.setContext(lc);
+        logcatAppender.setEncoder(encoder2);
+        logcatAppender.start();
+
+        // add the newly created appenders to the root logger;
+        // qualify Logger to disambiguate from org.slf4j.Logger
+        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.addAppender(fileAppender);
+        root.addAppender(logcatAppender);
+    }
+
+    private void logTest() {
+        org.slf4j.Logger log = LoggerFactory.getLogger(MainActivity.class);
+        for (int i = 0; i < 10; i++) {
+            log.info("hello world");
+        }
+    }
+}
